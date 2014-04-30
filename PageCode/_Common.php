d86a6c8f44bd4e95a7bceed9e8586ab0656d0aa2
Greg Bowler <greg.bowler@g105b.com>
2014-04-30 10:16:53 +0100

    Add hidden iframes for page sliding

--- PageCode/_Common.php
+++ PageCode/_Common.php
@@ -13,6 +13,7 @@ public function go($api, $dom, $template, $tool) {
 	if(PATH === "Index") {
 		$dom["body > header"]->remove();
 		$dom["body"]->removeAttribute("data-pjax");
+		$dom[".pageSlider"]->remove();
 	}
 }	
 }#
